26fc9e137693906f6d71f28146d4873a2eb9f0ab
Eugeneche <eugene.bizmail@gmail.com>
2022-09-19 00:30:46 +0200

    1

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,25 +6,25 @@ import * as style from "../style/_style.module.scss"
 
 import top from "../images/to_top.svg"
 
-const isBrowser = typeof window !== "undefined"
+//const isBrowser = typeof window !== "undefined"
 
 function Footer() {
   const year = new Date().getFullYear()
   const { t } = useTranslation()
-let locale
+/* let locale
   if (isBrowser) {
     locale = window.location.pathname
     
   }
-
+ */
   //const locale = `${window.location.origin}${window.location.pathname}`
 
-  console.log(locale)
+  //console.log(locale)
 
   return (
     <footer className={style.footer}>
       <div className={style.container}>
-        <LocalizedLink  className={style.buttonTop} to={locale}><img src={top} alt="to top button"></img></LocalizedLink>
+        <LocalizedLink  className={style.buttonTop} to="/"><img src={top} alt="to top button"></img></LocalizedLink>
         <p><Trans i18nKey="footer">{{ year }}</Trans></p>
 
       </div>
