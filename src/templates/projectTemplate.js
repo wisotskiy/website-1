982b97be77a12f4ff30a2b39070ba3e7e4c84d59
Eugeneche <eugene.bizmail@gmail.com>
2023-01-23 00:20:46 +0100

    2

--- src/templates/projectTemplate.js
+++ src/templates/projectTemplate.js
@@ -238,7 +238,7 @@ const Project = ({ data }) => {
             )
           })}
         </div> :
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/DivkNqXeAag" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/UWRf3OweYq0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             
         } 
                
