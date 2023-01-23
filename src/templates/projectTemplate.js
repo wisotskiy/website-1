3bfb6345f6d28bd01176fd64169073c9273d269a
Eugeneche <eugene.bizmail@gmail.com>
2023-01-23 01:16:10 +0100

    Embedding test

--- src/templates/projectTemplate.js
+++ src/templates/projectTemplate.js
@@ -238,7 +238,7 @@ const Project = ({ data }) => {
             )
           })}
         </div> :
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/UWRf3OweYq0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <Video videoSrcURL={data.mdx.frontmatter.link} />
             
         } 
                
